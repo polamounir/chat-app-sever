3d5092c961f62043256e088e47d8030b3a1dadff
polamounir <polamounir103@gmail.com>
2025-06-18 23:58:28 +0300

    fix db connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 
+
+
 const connectDB = async () => {
+  const dbUrl = process.env.DATABASE_URL;
+  console.log(dbUrl , "DDDDDDDDDDDDDD");
   try {
-    const conn = await mongoose.connect('mongodb://localhost:27017/chat-app');
+    // const conn = await mongoose.connect('mongodb://localhost:27017/chat-app');
+    const conn = await mongoose.connect(dbUrl);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
